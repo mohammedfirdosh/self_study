b3e1d5c1f083dcc1bdd28f96e79ceda91b9a8b61
mohammedfirdosh <123446124+mohammedfirdosh@users.noreply.github.com>
2025-01-05 19:11:15 +0530

    Update LC1768_Easy_Merge_Strings_Alternately.py
    37
    ms
    Beats
    51.00%

--- LeetCode/LC1768_Easy_Merge_Strings_Alternately.py
+++ LeetCode/LC1768_Easy_Merge_Strings_Alternately.py
@@ -2,12 +2,13 @@ class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         min_length = min(len(word1), len(word2))
         idx = 0
-        merged_word = ''
+        merged_word = []
         while idx < min_length:
-            merged_word += word1[idx] + word2[idx]
+            merged_word.extend([word1[idx], word2[idx]])
             idx += 1
+
         if min_length == len(word1):
-            merged_word += word2[idx:]
+            merged_word.extend(word2[idx:])
         else:
-            merged_word += word1[idx:]
-        return merged_word
+            merged_word.extend(word1[idx:])
+        return ''.join(merged_word)
