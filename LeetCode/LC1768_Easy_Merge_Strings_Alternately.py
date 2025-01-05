3201a1fd9f90acdb46f6bd06c28eec4d12671304
mohammedfirdosh <123446124+mohammedfirdosh@users.noreply.github.com>
2025-01-05 19:01:47 +0530

    Update LC1768_Easy_Merge_Strings_Alternately.py

--- LeetCode/LC1768_Easy_Merge_Strings_Alternately.py
+++ LeetCode/LC1768_Easy_Merge_Strings_Alternately.py
@@ -1,18 +1,13 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        w1_len = len(word1)
-        w2_len = len(word2)
-        if w1_len == 0:
-            return word2
-        if w2_len == 0:
-            return word1
-        result = ''.join(f"{w1}{w2}" for w1,w2 in zip(word1, word2))
-        if w1_len == w2_len:
-            return result
-        if w1_len > w2_len:
-            result = f"{result}{word1[-(w1_len - w2_len):]}"
-        elif w1_len < w2_len:
-            result = f"{result}{word2[-(w2_len - w1_len):]}"       
-
-        return result
-        
+        min_length = min(len(word1), len(word2))
+        idx = 0
+        merged_word = ''
+        while idx < min_length:
+            merged_word += word1[idx] + word2[idx]
+            idx += 1
+        if min_length == len(word1):
+            merged_word += word2[idx:]
+        else:
+            merged_word += word1[idx:]
+        return merged_word
