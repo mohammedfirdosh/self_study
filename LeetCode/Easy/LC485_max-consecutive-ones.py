c61465642b85c4e441498386838122fc7869e732
mohammedfirdosh <123446124+mohammedfirdosh@users.noreply.github.com>
2025-01-15 15:53:49 +0530

    Create LC485_max-consecutive-ones.py

---
+++ LeetCode/Easy/LC485_max-consecutive-ones.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        #https://leetcode.com/problems/max-consecutive-ones
+        # 8ms 92%
+        result, current_max = 0, 0
+
+        for num in nums:
+            if num:
+                current_max += 1
+            else:
+                result = max (result, current_max)
+                current_max = 0 
+        else:
+            if num:
+                result = max (result, current_max)
+        return result
