c6e78a74c5832706945e5420bdeac009cde00736
mohammedfirdosh <123446124+mohammedfirdosh@users.noreply.github.com>
2025-01-25 18:42:08 +0530

    Create LC832_flipping-an-image.py

---
+++ LeetCode/Easy/LC832_flipping-an-image.py
@@ -0,0 +1,10 @@
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        # https://leetcode.com/problems/flipping-an-image
+        for row in image:
+            l, r = 0, len(row) - 1
+            while l <= r:
+                row[l], row[r] = row[r]^1, row[l]^1
+                r -= 1
+                l += 1
+        return image
