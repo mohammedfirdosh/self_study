6f06c169934bfc4acb2fe228c1209802f023d96d
mohammedfirdosh <123446124+mohammedfirdosh@users.noreply.github.com>
2025-02-19 23:11:14 +0530

    Update LC852_peak-index-in-a-mountain-array.py

--- LeetCode/Medium/LC852_peak-index-in-a-mountain-array.py
+++ LeetCode/Medium/LC852_peak-index-in-a-mountain-array.py
@@ -1,6 +1,21 @@
 # 852 https://leetcode.com/problems/peak-index-in-a-mountain-array/
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        n = len(arr)
+        s, e = 1, n - 2
+        while s <= e:
+            m = s + (e - s) // 2
+            if arr[m - 1] < arr[m] > arr[m + 1]:
+                return m
+            elif arr[m - 1] > arr[m]:
+                e = m - 1
+
+            else:
+                s = m + 1
+        return -1
+
+
+        """
         low, high = 0, len(arr) - 1
 
         while low < high:
@@ -10,3 +25,4 @@ class Solution:
             else:
                 high = mid
         return low
+        """
