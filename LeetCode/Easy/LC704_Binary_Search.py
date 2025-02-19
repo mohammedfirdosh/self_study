bed04f8bf8c19b1d0408452ce531007f7db023d8
mohammedfirdosh <123446124+mohammedfirdosh@users.noreply.github.com>
2025-02-19 22:04:05 +0530

    Update LC704_Binary_Search.py

--- LeetCode/Easy/LC704_Binary_Search.py
+++ LeetCode/Easy/LC704_Binary_Search.py
@@ -1,6 +1,22 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
 
+        def bs(start, end):
+            if end < start:
+                return -1
+            mid = start + (end - start) // 2
+
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                return bs(start, mid - 1)
+            else:
+                return bs(mid + 1, end)
+
+        return bs(0, n - 1)
+
+        """
         # https://leetcode.com/problems/binary-search
         # 0ms 100%
         l, r = 0, len(nums)-1
@@ -15,3 +31,4 @@ class Solution:
             else:
                 r = m - 1
         return -1
+        """
