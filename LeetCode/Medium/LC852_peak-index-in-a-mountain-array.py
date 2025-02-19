a68a3bbdd53800a9b26fe54efaff6793aa5a6664
mohammedfirdosh <123446124+mohammedfirdosh@users.noreply.github.com>
2025-02-19 22:51:11 +0530

    Create LC852_peak-index-in-a-mountain-array.py

---
+++ LeetCode/Medium/LC852_peak-index-in-a-mountain-array.py
@@ -0,0 +1,12 @@
+# 852 https://leetcode.com/problems/peak-index-in-a-mountain-array/
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        low, high = 0, len(arr) - 1
+
+        while low < high:
+            mid = (low + high) // 2
+            if arr[mid] < arr[mid + 1]:
+                low = mid + 1
+            else:
+                high = mid
+        return low
