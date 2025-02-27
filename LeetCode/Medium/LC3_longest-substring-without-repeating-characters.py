876d059a4a7a2315c03e9e63cd24b1c057e67633
mohammedfirdosh <123446124+mohammedfirdosh@users.noreply.github.com>
2025-02-27 22:33:44 +0530

    Update LC3_longest-substring-without-repeating-characters.py

--- LeetCode/Medium/LC3_longest-substring-without-repeating-characters.py
+++ LeetCode/Medium/LC3_longest-substring-without-repeating-characters.py
@@ -1,3 +1,19 @@
+# 3 https://leetcode.com/problems/longest-substring-without-repeating-characters/
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        char_set = set()
+        l = 0
+        max_len = 0
+
+        for r in range(len(s)):
+            while s[r] in char_set:
+                char_set.remove(s[l])
+                l += 1
+            char_set.add(s[r])
+            max_len = max(max_len, r - l + 1)
+        return max_len
+
+"""
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         # https://leetcode.com/problems/longest-substring-without-repeating-characters
@@ -13,3 +29,4 @@ class Solution:
             max_length = max(max_length, end - start + 1)
             #print(f"char_map: {char_map} | max_length: {max_length} | start: {start} | end: {end}")
         return max_length
+"""
