afcad651934394cb19d3003e67402f1922a6a4ec
mohammedfirdosh <123446124+mohammedfirdosh@users.noreply.github.com>
2025-03-01 21:42:24 +0530

    Create merge_sort.py

---
+++ LeetCode/Sorting/merge_sort.py
@@ -0,0 +1,37 @@
+# Merge sort
+def mergeSort(arr, start, end):
+    if start < end:
+        mid = start + (end - start) //2
+
+        mergeSort(arr, start, mid) # Left
+        mergeSort(arr, mid +1 , end) # Right
+
+        merge(arr, start, mid, end)
+
+def merge(arr, start, mid, end):
+    tmp = list()
+    i, j = start, mid +1
+    while i <=mid and j <= end:
+        if arr[i] <= arr[j]:
+            tmp.append(arr[i])
+            i +=1
+        else:
+            tmp.append(arr[j])
+            j += 1
+    while i<= mid:
+        tmp.append(arr[i])
+        i += 1
+    while j<= end:
+        tmp.append(arr[j])
+        j += 1
+    
+    for i in range(len(tmp)):
+        arr[i + start] = tmp[i]
+
+if __name__ == '__main__':
+    arr = [12, 31, 35, 8, 32, 17]
+    print(f"Unsorted list: {arr}")
+    n = len(arr)
+    start, end = 0, n-1
+    mergeSort(arr, start, end)
+    print(f"Sorted list: {arr}")
